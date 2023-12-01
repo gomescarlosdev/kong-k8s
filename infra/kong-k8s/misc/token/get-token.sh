dcf3f5201d2540d32175ecaffecdc2c87ad2498b
gomescarlosdev <carlos.henrique.pro@outlook.com>
2023-12-01 14:47:35 -0300

    feat:(kong): :sparkles: deploy openid

--- infra/kong-k8s/misc/token/get-token.sh
+++ infra/kong-k8s/misc/token/get-token.sh
@@ -5,7 +5,7 @@ curl --location --request POST 'http://keycloak.iam/realms/bets/protocol/openid-
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'client_id=kong' \
 --data-urlencode 'grant_type=password' \
---data-urlencode 'username=maria' \
---data-urlencode 'password=maria' \
---data-urlencode 'client_secret=ohSN0VS9Qtdi5KCLmkj52jGFU91FQJn2' \
+--data-urlencode 'username=carlosh' \
+--data-urlencode 'password=carlosh' \
+--data-urlencode 'client_secret=uoaymTI69vxDCIf6IxczqdwrSlUF6EAp' \
 --data-urlencode 'scope=openid'
